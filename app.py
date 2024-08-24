a903b2339b0aec79149e8c4ebee9dcc9263cfb97
RenderHaven <vikrambalai1002@gmail.com>
2024-08-24 18:37:17 +0530

    changed

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/hii')
+def hii():
+    return "land le le"
 
 if __name__ == '__main__':
     app.run(debug=True)
